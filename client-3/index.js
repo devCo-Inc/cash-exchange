0343966b195549c0cc7e1888c778acf561ee2a2c
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-08-15 20:26:46 -0700

    :construction: Working on Client-3

---
+++ client-3/index.js
@@ -0,0 +1,28 @@
+const { io } = require('socket.io-client');
+const events = io('ws://localhost:3000');
+const { chance, eventListner } = require('../utils');
+
+function sendEvent() {
+  const event = {
+    name: chance.name(),
+    account: chance.integer({ min: 1, max: 100 }),
+    amount: chance.integer({ min: 1, max: 1000 }),
+    date: chance.date({ string: true }),
+    type: chance.pickone(['deposit', 'withdraw']),
+    transationId: chance.guid(),
+  };
+
+  const payload = {
+    event: 'deposit',
+    date: event.date,
+    account: event.account,
+    amount: event.amount,
+    transationId: event.transationId,
+  };
+  console.log(
+    `Client 3 - ${event.type} - ${event.date} - ${event.account} - ${event.amount}`
+  );
+  events.emit('deposit', payload);
+}
+sendEvent();
+eventListner(events);
