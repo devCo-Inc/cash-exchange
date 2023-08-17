5be77cb57bec826ef9c1573b0a90c046db56565a
Lacey W <lacey.m.washington@gmail.com>
2023-08-16 22:54:07 -0500

    added rooms and target for clients 1,3, and 4 we need to make a handle recieve for each client and for the hub

--- client-1/handlers.js
+++ client-1/handlers.js
@@ -1,21 +1,56 @@
 'use strict';
+'use strict';
+const { EventNames, chance } = require('../utilities');
 
-const { chance } = require('chance');
+const event = {
+  name: chance.name(),
+  account: chance.integer({ min: 1, max: 100 }),
+  amount: chance.dollar(),
+  date: chance.date({ string: true }),
+  type: chance.pickone(['deposit', 'withdraw']),
+  transactionId: chance.guid(),
+};
 
-function createPayload() {
-  const event = {
-    name: chance.name(),
-    account: chance.integer({min:1, max: 100}),
-    amount: chance.date({string: true }),
-    type: chance.pickone(['deposit', 'withdraw']),
-    transactionId: chance.guid(),
-  };
+const payload = {
+  event: 'deposit',
+  date: event.date,
+  account: event.account,
+  amount: event.amount,
+  transactionId: event.transactionId,
+  client: EventNames.client3
+};
 
-  const payload = {
-    event: event.type,
-    date: event.date,
-    account: event.account,
-    amount: event.amount,
-    transactionId: event.transactionId
-  };
+
+function sendEvent(client) {
+  console.log(
+    `Client 3 - ${event.type} - ${event.date} - ${event.account} - ${event.amount} , ${event.transactionId}`
+  );
+  client.emit(EventNames.connect, payload.client);
+  client.emit(EventNames.send, {target:EventNames.client3, ...payload});
+  client.emit(EventNames.send, {target:EventNames.client2, ...payload});
 }
+
+ 
+
+function startClient(client) {
+  console.log('Client 3 started ...');
+  let loopCounter = 0;
+  const maxLoopIterations = 10; // Set the desired number of iterations
+
+  function ready() {
+    if (loopCounter >= maxLoopIterations) {
+      console.log('Max Transaction Meet For The Day.');
+      return; // Exit the loop
+    }
+    sendEvent(client);
+    setTimeout(() => {
+      loopCounter++;
+      ready(); // Continue the loop
+    }, chance.integer({ min: 1000, max: 5000 }));
+  }
+  ready();
+}
+
+module.exports = {
+  startClient,
+};
--- client-4/index.js
+++ client-4/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const { io } = require('socket.io-client');
-const {EventNames} = require('../utilities');
 const {startClient} = require('./handlers');
 
 const client = io('ws//localhost:3000');
